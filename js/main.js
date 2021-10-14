f7527193146077026166457868a0698a640f5994
pusyak <pusyak04@gmail.com>
2021-10-14 13:40:24 +0300

    dada

--- js/main.js
+++ js/main.js
@@ -1,15 +1,16 @@
+$('.popup-form_input, .popup-form_textarea').on('input', function() { 
+  if ($(this).val().replace(' ', '') != '') {
+    $(this).addClass('_active')
+  } else {
+    $(this).removeClass('_active')
+  }
+});
 
+$('.popup_close').click(function (e) {
+  e.preventDefault()
 
-
-
-
-
-
-
-
-
-
-
+  $.fancybox.close()
+})
 
 
 
